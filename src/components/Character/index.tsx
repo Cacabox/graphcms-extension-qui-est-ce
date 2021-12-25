ef8344eef68947c2ed71cdfc03732bb65bf0b908
Alexandre Le Foll <alexandre@le-foll.fr>
2021-12-25 11:41:38 +1100

    Version 1.0.3

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -47,7 +47,7 @@ export const Character = () => {
             setBounds({ left, right });
 
             setDefaultPosition({
-                x: value !== undefined ? -(-left * value) : (boxWidth / 2) - (width / 2),
+                x: value !== null ? -(-left * value) : (boxWidth / 2) - (width / 2),
                 y: 0,
             });
         }
